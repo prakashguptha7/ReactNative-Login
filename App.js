4cbcf106e98156aee3fc4c5581a11659bab920c7
sathishrajendiran <sathish@naberly.com>
2020-06-04 11:36:31 +0530

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,145 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image,
+  Header,
+  Alert
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class LoginView extends Component {
+
+  constructor(props) {
+    super(props);
+    state = {
+      email   : '',
+      password: '',
+    }
+  }
+
+  // isValid() {
+  //   const { email, password } = this.state;
+  //   let valid = false;
+
+  //   if (email.length > 0 && password.length > 0) {
+  //     valid = true;
+  //   }
+
+  //   if (email.length === 0 || email === '') {
+  //     this.setState({ error: 'You must enter an email address' });
+  //   } else if (password.length === 0) {
+  //     this.setState({ error: 'You must enter a password' });
+  //   }
+
+  //   return valid;
+  // }
+
+  onSignIn() {
+    const {email, password} = this.state;
+
+    if (email.length > 0 && password.length > 0) {
+      Alert.alert("Alert", `User email is${email}`);
+    }else if(email.length === 0 || email === ''|| email === null){
+      Alert.alert("Alert", `You must enter an email address`);
+    }else if(password.length === 0 || password === ''|| password === null){
+      Alert.alert("Alert", `You must enter a password`);
+    }else{
+      Alert.alert("Alert", `Something Went Wrong`);
+    }
+  }
+
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed "+viewId);
+  }
+
+  render() {
+    return ( 
+      <View style={styles.container}>
+        <Text h2 style={styles.heading}>Login</Text>
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+        <TouchableHighlight  style={[styles.buttonContainer, styles.loginButton]} onPress={this.onSignIn.bind(this)}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+            <Text>Register</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
   },
+  heading:{
+   fontSize:20,
+   marginBottom:10,
+   fontWeight:"bold",
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: 'white',
+  }
 });
+ 
